87c4de3c52ef21ec5c6086ddd8a410e192c37916
Glitch (open-lovely-doom) <none>
2020-12-09 02:24:03 +0000

    🏟🐩 Updated with Glitch: using template instead of html pages

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const year = 2020;
-const days = [
+const days = [  "zero",
   "one", "two", "three", "four", "five",
   "six", "seven", "eight", "nine", "ten",
   "eleven", "twelve", "thirteen", "fourteen", "fifteen",
@@ -31,37 +31,6 @@ const days = [
   "twentyone", "twentytwo", "twentythree", "twentyfour", "twentyfive"
 ];
 
-// bind 25 days of html files, and post functions for both parts of each
-for (let d = 1; d <= 25; d++) {
-  // string day, leading zero
-  const dd = "" + d;
-  const day = dd.padStart(2, "0");
-
-  app.get("/day" + day, function(request, response) {
-    response.sendFile(__dirname + "/views/day" + day + ".html");
-  });
-
-  for (let p = 1; p <= 2; p++) {
-    // string part
-    const part = "" + p;
-    app.post("/day" + day + "part" + p, function(request, response) {
-      // name of the timer
-      const timer = "day " + dd + ", part " + part;
-      console.time(timer);
-
-      // pass in string of day number and part, and send the request body's imput param to that function
-      const answer = f.funs(dd, part)(request.body.input);
-
-      console.log(answer);
-      console.timeEnd(timer);
-
-      // respond with the json that includes the answer
-      response.status(200).json({ output: answer });
-    });
-  }
-}
-
-// *beta* view template
 let dayTemplate = "";
 fs.readFile(__dirname + "/views/day.ntl", function (err, content) {
   if (err) {
@@ -83,18 +52,49 @@ app.engine('ntl', (filePath, options, callback) => { // define the template engi
 });
 app.set('views', './views'); // specify the views directory
 app.set('view engine', 'ntl'); // register the template engine
-//TODO: bind this in the for loop above for each day, change html bind to redirect
-app.get('/day', (req, res) => {
-  res.render('day', { 
-    title: 'one', 
-    description: "AOC " + year + ", day one",
-    prev_url: "/",
-    next_url: "/day/02",
-    year: year,
-    day: "01",
-    day_num: "1"
+
+// bind 25 days of html files, and post functions for both parts of each
+for (let d = 1; d <= 25; d++) {
+  // string day, leading zero
+  const dd = "" + d;
+  const day = dd.padStart(2, "0");
+
+  app.get("/day" + day, function(request, response) {
+    //response.sendFile(__dirname + "/views/day" + day + ".html");
+    response.redirect("/day/" + day);
   });
-});
+  
+  app.get('/day/' + day, (req, res) => {
+    res.render('day', { 
+      title: days[d], 
+      description: "AOC " + year + ", day " + days[d],
+      prev_url: d <= 1 ? "/" : "/day/" + ("" + (d - 1)).padStart(2, "0"),
+      next_url: d >= 25 ? "/" : "/day/" + ("" + (d + 1)).padStart(2, "0"),
+      year: year,
+      day: day,
+      day_num: dd
+    });
+  });
+  
+  for (let p = 1; p <= 2; p++) {
+    // string part
+    const part = "" + p;
+    app.post("/day" + day + "part" + p, function(request, response) {
+      // name of the timer
+      const timer = "day " + dd + ", part " + part;
+      console.time(timer);
+
+      // pass in string of day number and part, and send the request body's imput param to that function
+      const answer = f.funs(dd, part)(request.body.input);
+
+      console.log(answer);
+      console.timeEnd(timer);
+
+      // respond with the json that includes the answer
+      response.status(200).json({ output: answer });
+    });
+  }
+}
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
